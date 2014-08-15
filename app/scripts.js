57207a563e5c74c86559eb7c5362346c9be3eff1
owenmead <owenmead@gmail.com>
2014-08-15 12:57:12 -0700

    Now we're getting somewhere

--- app/scripts.js
+++ app/scripts.js
@@ -1,21 +1,61 @@
 angular.module('pikaApp', [])
-.directive('pikadaytime', function(){
+.controller('myCtrl', function($scope) {
+  $scope.theValue = null; //new Date();
+})
+.directive('pikadaytime', function($filter){
   // Runs during compile
   return {
-    // name: '',
-    // priority: 1,
-    // terminal: true,
-    // scope: {}, // {} = isolate, true = child, false/undefined = no change
-    // controller: function($scope, $element, $attrs, $transclude) {},
-    // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-    restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
-    // template: '',
-    // templateUrl: '',
-    // replace: true,
-    // transclude: true,
-    // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-    link: function($scope, iElm, iAttrs, controller) {
-      iElm[0].innerHTML = 'stuff ';
+    require: 'ngModel',
+    restrict: 'E', // E = Element
+    template: '<input type="text"><br>{{theValue}}',
+
+    link: function(scope, element, attrs, ngModel) {
+      var inputDOM = element.find('input');
+
+      // Setup Pikaday
+      pikadayOpts = {
+        field: inputDOM[0],
+        format: 'D MMM YYYY',
+        onSelect: function(selectedDate) {
+          scope.$apply(function() {
+            ngModel.$setViewValue(selectedDate);
+          });
+        }
+      }
+      var picker = new Pikaday(pikadayOpts);
+
+      // Clean up Pikaday when scope is destroyed
+      scope.$on('$destroy', function() {
+        picker.destroy();
+      });
+
+       inputDOM.on('change', function(){
+        if (inputDOM.val() === '' || !inputDOM.val()) {
+          ngModel.$setValidity('pikadaytime', true);
+          scope.$apply();
+        }
+       })
+
+      // Incoming from model changes
+      ngModel.$formatters.push(function(value) {
+        if (angular.isDate(value)) {
+          return value;
+        }
+        return '';
+      })
+
+
+      // Called when the view needs to be updated
+      // Specify how UI should be updated
+      ngModel.$render = function() {
+        if (angular.isDate(ngModel.$viewValue)) {
+          picker.setDate(ngModel.$viewValue, true);
+        } else {
+          inputDOM.val('');
+          picker.setDate(null, true);
+        }
+      };
+
     }
   };
 });
