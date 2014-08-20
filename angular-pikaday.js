825ef1f1ea41ef039429d8115db1b4615806b461
owenmead <owenmead@gmail.com>
2014-08-20 16:03:14 -0700

    Provide a way to update the Pikaday overrides

--- angular-pikaday.js
+++ angular-pikaday.js
@@ -1,7 +1,18 @@
 'use strict';
 
 angular.module('angular-pikaday', [])
-.directive('pikaday', function($filter){
+.constant('pikaconfig', {
+  // Simple observer pattern to allow outsiders to change Pikaday options
+  pikadayObservers: [],
+  updatePikadayOptions: function() {
+    angular.forEach(this.pikadayObservers, function(callback) {
+      callback();
+    });
+  },
+
+  option_overrides: {}
+})
+.directive('pikaday', function(pikaconfig){
   return {
     require: 'ngModel',
     restrict: 'E', // E = Element
@@ -11,7 +22,7 @@ angular.module('angular-pikaday', [])
       var inputDOM = element.find('input');
 
       // Setup Pikaday
-      var picker = new Pikaday({
+      ngModel.picker = new Pikaday({
         field: inputDOM[0],
         onSelect: function(selectedDate) {
           scope.$apply(function() {
@@ -20,17 +31,29 @@ angular.module('angular-pikaday', [])
         }
       });
 
-      // Allow date format to be set and dynamically changed
-      attrs.$observe('format', function(format) {
-        if (format) {
-          picker._o.format = format;
-          ngModel.$render();
-        }
-      });
+      // We need to update our options
+      // Using observer pattern so as not to pollute scope or add a bunch of watches
+      var optionUpdateCallback = function() {
+        angular.extend(ngModel.picker._o, pikaconfig.option_overrides);
+        ngModel.$render();
+      }
+      pikaconfig.pikadayObservers.push(optionUpdateCallback);
 
       // Clean up Pikaday when this directive instance is destroyed
       scope.$on('$destroy', function() {
-        picker.destroy();
+        ngModel.picker.destroy();
+        // Remove self from observables
+        pikaconfig.pikadayObservers.splice(
+            pikaconfig.pikadayObservers.indexOf(optionUpdateCallback), 1
+          );
+      });
+
+      // Allow date format to be set and dynamically changed
+      attrs.$observe('format', function(format) {
+        if (format) {
+          ngModel.picker._o.format = format;
+          ngModel.$render();
+        }
       });
 
       // Incoming from model changes, revalidate and force a date type
@@ -54,9 +77,9 @@ angular.module('angular-pikaday', [])
       // Handle DOM events for our date picker
       inputDOM.on('blur', function(){
         scope.$apply(function() {
-          picker.hide();
-          ngModel.$setViewValue(picker.getDate());
-          ngModel.$setValidity('pikaday', (inputDOM.val().length === 0 || picker.getDate() !== null));
+          ngModel.picker.hide();
+          ngModel.$setViewValue(ngModel.picker.getDate());
+          ngModel.$setValidity('pikaday', (inputDOM.val().length === 0 || ngModel.picker.getDate() !== null));
         });
       });
       inputDOM.on('keyup', function(){
@@ -71,10 +94,10 @@ angular.module('angular-pikaday', [])
       // Called when the view needs to be updated, again ensure actual date
       ngModel.$render = function() {
         if (angular.isDate(ngModel.$viewValue)) {
-          picker.setDate(ngModel.$viewValue, true);
+          ngModel.picker.setDate(ngModel.$viewValue, true);
         } else {
           inputDOM.val('');
-          picker.setDate(null, true);
+          ngModel.picker.setDate(null, true);
         }
       };
     }
--- app/scripts.js
+++ app/scripts.js
@@ -1,6 +1,36 @@
 'use strict';
 
 angular.module('pikaApp', ['angular-pikaday'])
-.controller('myCtrl', function($scope) {
+.controller('myCtrl', function($scope, $rootScope, pikaconfig) {
   $scope.theValue = new Date();
+  $scope.currentLocal = 'en-ca';
+  $scope.changeLocal = function() {
+    $scope.currentLocal = $scope.currentLocal === 'en-ca' ? 'es' : 'en-ca';
+    moment.locale($scope.currentLocal);
+    $rootScope.$broadcast('updateLocale');
+  }
+
+  $scope.$on('updateLocale', function() {
+    pikaconfig.option_overrides = {
+      i18n: {
+        previousMonth : 'Previous Month',
+        nextMonth     : 'Next Month',
+        months        : moment.months(),
+        weekdays      : moment.weekdays(),
+        weekdaysShort : moment.weekdaysShort()
+      }
+    }
+    pikaconfig.updatePikadayOptions();
+  })
+
+  $scope.pikaconfig = pikaconfig;
+})
+
+
+.config(function(pikaconfig) {
+  pikaconfig.onRender = function() {
+    console.log('render!');
+    this.picker._o.i18n.months = moment.months();
+    //console.log("DA RENDER!", this.picker);
+  };
 });
